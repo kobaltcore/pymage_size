6a4d830b24145cb24a86e0228f72ce8f39ae42d5
cobaltcore <cobaltcore@yandex.com>
2023-01-27 00:00:59 +0100

    Fix data access

--- pymage_size/pymage_size.py
+++ pymage_size/pymage_size.py
@@ -9,7 +9,7 @@ def get_image_size(file):
     file.seek(0, 2)
     size = file.tell()
     file.seek(0)
-    data = file.read(512)
+    data = file.read(26)
 
     for format_class in ImageFormat.__subclasses__():
         if format_class.detect(file, size, data):
@@ -249,7 +249,7 @@ class AVIFFormat(ImageFormat):
         return data[0:3] == b"\x00\x00\x00" and data[4:12] == b"ftypavif"
 
     def parse(self):
-        data = self.data
+        data = self.data + self.file.read(512)
         ispe_index = data.index(b"\x00\x00\x00\x14ispe")
         data = data[ispe_index : ispe_index + 20]
         width = struct.unpack(">I", data[12:16])[0]
